97cc699c4d8f9914f79e41a0075cae62b0bed692
Omar Almootassem <218omar.a@gmail.com>
2017-03-19 13:49:28 -0400

    set up get and post routes for mongo

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+var Data = mongoose.model('Data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +12,13 @@ router.post('/data', function(req, res, next){
 	var data = new Data(req.body);
 
 	data.save(function(err, data){
+		if (err) throw err;
+		res.json(data);
+	});
+});
+
+router.get('/data', function(req, res){
+	Data.find(function(err, data){
 		if (err) throw err;
 		res.json(data);
 	});
